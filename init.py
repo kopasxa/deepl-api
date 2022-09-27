76d71f4b561539650b97c7cba396b26cedb61123
Pavel <pavelcorolenko535@gmail.com>
2022-09-28 01:30:42 +0300

    germany relaese

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ import deepl
 import config
 from db import database
 from Parser import Parser
+import threading
 
 auth_key = config.DEEPL_KEY
 translator = deepl.Translator(auth_key)
@@ -10,9 +11,10 @@ db = database()
 sources = db.get_sources_articles()
 
 for source in sources:
-    if source[1] == "France24":
+    if source[1]:# == "DW":
         parser = Parser(source[4])
-        parser.get_all_articles(source[2])
+        threading.Thread(target=parser.get_all_articles, args=(source[2],)).start()
+        #parser.get_all_articles(source[2])
 
 #try:
 #    with open("from_text.txt", "r") as f:
